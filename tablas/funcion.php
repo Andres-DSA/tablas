634fc9dcc45af97d7b6da5bf113837c116906dd1
Andres-DSA <163669356+Andres-DSA@users.noreply.github.com>
2024-04-05 17:05:56 -0500

    Delete tablas directory
    no me gusto

--- tablas/funcion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-        echo "<div class='php'>";
-        $numero = $_POST["numero"];
-        for($i=1; $i<=50; $i++){
-            echo "<p class='p_php'>".$numero." X ".$i." = ".$i*$numero."</p>"."<br>";
-        }
-        echo "</div>";
